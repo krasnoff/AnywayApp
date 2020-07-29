c54b4080039402e5cdc19c2b30ea64377d2fa0e8
kobi krasnoff <krasnoff.kobi@gmail.com>
2020-07-29 09:13:23 +0300

    add geolocation

--- PlayerScreen/PlayerScreen.js
+++ PlayerScreen/PlayerScreen.js
@@ -2,39 +2,43 @@ import React, { Component } from 'react';
 import { View, Text, Image, AppState, Platform } from 'react-native';
 import { connect } from 'react-redux';
 import styles from '../style/styles';
+import Geolocation from '@react-native-community/geolocation';
 
 class PlayerScreen extends Component {
     static navigationOptions = {
         title: 'Player - Kobi Krasnoff',
     };
 
-    myProgressTimer = null;
-    myTimer = null;
-
     state = {
-        isPlaying: false,
-        appState: AppState.currentState,
-        progress: 0.0,
-        duration: 0.0,
-        durationFormat: '00:00',
-        durationProgress: '00:00',
-        currentTrack: {
-            artist: "",
-            artwork: "https://previews.123rf.com/images/alekseyvanin/alekseyvanin1705/alekseyvanin170501394/77840540-feed-icon-vector-rss-solid-logo-illustration-colorful-pictogram-isolated-on-white.jpg",
-            id: "",
-            title: "",
-            url: ""
-        }
-    }
+        initialPosition: null,
+        lastPosition: null,
+    };
+
+    watchID = null;
     
     componentDidMount() {
-        
+        Geolocation.getCurrentPosition(
+            position => {
+              this.setState({initialPosition: position});
+            },
+            error => Alert.alert('Error', JSON.stringify(error)),
+            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+          );
+          this.watchID = Geolocation.watchPosition(position => {
+            this.setState({lastPosition: position});
+          });
+    }
+
+    componentWillUnmount() {
+        this.watchID != null && Geolocation.clearWatch(this.watchID);
     }
     
     render() {
         return (
             <View style={styles.container}>
-                <Text>Hello</Text>
+                <Text>Hello 2</Text>
+                <Text>latitude: {this.state.initialPosition ? this.state.initialPosition.coords.latitude : null}, 
+                longitude: {this.state.initialPosition ? this.state.initialPosition.coords.longitude : null}</Text>
             </View>
         );
     }
